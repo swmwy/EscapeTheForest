e9df6c27637708f41c450b19c3358df0fbb2c86b
swmwy <149204419+swmwy@users.noreply.github.com>
2024-10-08 16:30:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from time import sleep
 from random import randint
 
-map1 
+map1 = [["Forest", "Forest", "Forest", "Forest", "Exit"],
+        ["Cabin", "Forest", "Forest", "Forest", "Forest"],
+        ["Forest", "Forest", "Forest", "Forest", "Forest"],
+        ["Mountain", "Mountain", "Forest", "Forest", "Forest"],
+        ["Mountain", "Mountain", "Forest", "Forest", "Forest"]]
+
