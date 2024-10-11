00225170089cedcac817f214ace60c53648bde29
swmwy <149204419+swmwy@users.noreply.github.com>
2024-10-11 21:12:24 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ map1 = [["Forest", "Forest", "Forest", "Forest", "Exit"],
         ["Mountain", "Mountain", "Forest", "Forest", "Forest"],
         ["Mountain", "Mountain", "Forest", "Forest", "Forest"]]
 
+def clearTerminal():
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
 print("Welcome to Escape the Forest")
-os.system('cls' if os.name == 'nt' else "printf '\033c'")
+sleep(5)
+clearTerminal()
+
+print("You can:\n")
+print(" ----------------\n",
+      "|1. Play       |\n",
+      "----------------\n",
+      "|2. See Credits|\n"
+      " ----------------\n")
