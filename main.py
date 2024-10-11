9974e3ae8bbe7346cea3ef67af8268a9f7147081
swmwy <149204419+swmwy@users.noreply.github.com>
2024-10-11 16:57:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from random import randint
+import os
 
 map1 = [["Forest", "Forest", "Forest", "Forest", "Exit"],
         ["Cabin", "Forest", "Forest", "Forest", "Forest"],
@@ -7,3 +8,5 @@ map1 = [["Forest", "Forest", "Forest", "Forest", "Exit"],
         ["Mountain", "Mountain", "Forest", "Forest", "Forest"],
         ["Mountain", "Mountain", "Forest", "Forest", "Forest"]]
 
+print("Welcome to Escape the Forest")
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
